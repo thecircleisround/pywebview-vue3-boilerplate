fc76ffe5520a8aaaeccaedcbc8a21acff44fcb24
thecircleisround <roundisthecircle@gmail.com>
2022-06-09 00:46:51 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <h2>Count {{ counter }} </h2>
+    <input type="text">
+    <input type="password">
+    <input type="checkbox">
+    <br>
+    <button @click="myBtn">Increase Count</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  data: function(){
+    return { 
+      counter: 0
+      }
+  },
+  methods: {
+    myBtn() {
+      this.counter ++ 
+      console.log(window.pywebview.api.showCount(this.counter));
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/index.py
@@ -0,0 +1,49 @@
+import os
+import threading
+import webview
+
+from time import time
+
+
+class Api:
+    def fullscreen(self):
+        webview.windows[0].toggle_fullscreen()
+
+    def save_content(self, content):
+        filename = webview.windows[0].create_file_dialog(webview.SAVE_DIALOG)
+        if not filename:
+            return
+
+        with open(filename, 'w') as f:
+            f.write(content)
+
+    def ls(self):
+        return os.listdir('.')
+
+    def showCount(self, count):
+        print(f'Current Count{count}')
+        return
+
+
+def get_entrypoint():
+    def exists(path):
+        return os.path.exists(os.path.join(os.path.dirname(__file__), path))
+
+    if exists('../gui/index.html'):  # unfrozen development
+        return '../gui/index.html'
+
+    if exists('../Resources/gui/index.html'):  # frozen py2app
+        return '../Resources/gui/index.html'
+
+    if exists('./gui/index.html'):
+        return './gui/index.html'
+
+    raise Exception('No index.html found')
+
+
+entry = get_entrypoint()
+
+if __name__ == '__main__':
+    api = Api()
+    window = webview.create_window('pywebview-vue boilerplate', entry, js_api=api)
+    webview.start(debug=True)
