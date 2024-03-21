135eb60fd25ba289718d539b5c07defae1e79155
Antonio Allen <roundisthecircle@gmail.com>
2024-03-21 12:49:01 -0400

    Update build-macos.py
    Revised tree function

--- build-macos.py
+++ build-macos.py
@@ -1,31 +1,42 @@
 import os
-import py2app
 import shutil
+import py2app 
 
 from distutils.core import setup
 
 def tree(src):
-    return [(root, map(lambda f: os.path.join(root, f), files))
-        for (root, dirs, files) in os.walk(os.path.normpath(src))]
-
+    first_pass = True
+    for root, dirs, files in os.walk(os.path.normpath(src)):
+        if first_pass:
+            yield ('', [os.path.join(root, f) for f in files])
+            first_pass = False
+        else:
+            relative_root = os.path.relpath(root, src)
+            yield (relative_root, [os.path.join(root, f) for f in files])
 
 if os.path.exists('build'):
     shutil.rmtree('build')
 
-if os.path.exists('dist/index.app'):
-    shutil.rmtree('dist/index.app')
+if os.path.exists('dist/main.app'):
+    shutil.rmtree('dist/main.app')
 
 ENTRY_POINT = ['src/index.py']
 
-DATA_FILES = tree('dist')
+DATA_FILES = list(tree('gui'))
+
+# Include additional files here
+# DATA_FILES.extend([
+#     ('', [])
+# ])
+
 OPTIONS = {
     'argv_emulation': False,
     'strip': True,
-    'iconfile': 'src/assets/logo.icns',
     'includes': ['WebKit', 'Foundation', 'webview', 'pkg_resources.py2_warn']
 }
 
 setup(
+    name="App Name",
     app=ENTRY_POINT,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
